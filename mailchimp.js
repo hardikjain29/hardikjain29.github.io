9901550003a855267758cd537f93c103b616c1da
Hardik Jain <29hdkjain@gmail.com>
2020-03-06 21:19:45 +0530

    Integrate mailchimp everywhere

--- mailchimp.js
+++ mailchimp.js
@@ -5,11 +5,63 @@ $(document).ready(function () {
     $('form button[type="submit"]').bind('click', function (event) {
       if (event) event.preventDefault();
       if (validateEmail($('#mce-EMAIL').val())) {
-          register($form)
+          register($form, '#mc-embedded-subscribe', '#message', '#mce-EMAIL', 'Early Access', true)
       } else {
           $('#message').empty();
           $('#message').append('<div class="ta st message-right">Something seems off with the email.</div>');
           $('#mce-EMAIL').removeClass('tt').addClass('z');
+      }
+    })
+  }
+  var $basicForm = $('#mc-embedded-subscribe-form-basic')
+  if ($basicForm.length > 0) {
+    $('#mc-embedded-subscribe-form-basic button[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault();
+      if (validateEmail($('#mce-EMAIL-basic').val())) {
+          register($basicForm, '#mc-embedded-subscribe-basic', '#message-basic', '#mce-EMAIL-basic', 'Let\'s try', false);
+      } else {
+          $('#message-basic').empty();
+          $('#message-basic').append('<div class="ta st message-right">Something seems off with the email.</div>');
+          $('#mce-EMAIL-basic').removeClass('tt').addClass('z');
+      }
+    })
+  }
+  var $businessForm = $('#mc-embedded-subscribe-form-business')
+  if ($businessForm.length > 0) {
+    $('#mc-embedded-subscribe-form-business button[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault();
+      if (validateEmail($('#mce-EMAIL-business').val())) {
+          register($businessForm, '#mc-embedded-subscribe-business', '#message-business', '#mce-EMAIL-business', 'Let\'s try', false);
+      } else {
+          $('#message-business').empty();
+          $('#message-business').append('<div class="ta st message-right">Something seems off with the email.</div>');
+          $('#mce-EMAIL-business').removeClass('tt').addClass('z');
+      }
+    })
+  }
+  var $enterpriseForm = $('#mc-embedded-subscribe-form-enterprise')
+  if ($enterpriseForm.length > 0) {
+    $('#mc-embedded-subscribe-form-enterprise button[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault();
+      if (validateEmail($('#mce-EMAIL-enterprise').val())) {
+          register($enterpriseForm, '#mc-embedded-subscribe-enterprise', '#message-enterprise', '#mce-EMAIL-enterprise', 'Let\'s try', false);
+      } else {
+          $('#message-enterprise').empty();
+          $('#message-enterprise').append('<div class="ta st message-right">Something seems off with the email.</div>');
+          $('#mce-EMAIL-enterprise').removeClass('tt').addClass('z');
+      }
+    })
+  }
+  var $footerForm = $('#mc-embedded-subscribe-form-footer')
+  if ($footerForm.length > 0) {
+    $('#mc-embedded-subscribe-form-footer button[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault();
+      if (validateEmail($('#mce-EMAIL-footer').val())) {
+          register($footerForm, '#mc-embedded-subscribe-footer', '#message-footer', '#mce-EMAIL-footer', 'Let\'s try', false);
+      } else {
+          $('#message-footer').empty();
+          $('#message-footer').append('<div class="ta st message-right">Something seems off with the email.</div>');
+          $('#mce-EMAIL-footer').removeClass('tt').addClass('z');
       }
     })
   }
@@ -22,8 +74,8 @@ function validateEmail(email)
   }
 
 
-function register($form) {
-  $('#mc-embedded-subscribe').text('Listing you...');
+function register($form, buttonId, messageContainerId, inputId, buttonText, isRight) {
+  $(buttonId).text('Listing you...');
   $.ajax({
     type: $form.attr('method'),
     url: $form.attr('action'),
@@ -32,29 +84,29 @@ function register($form) {
     dataType: 'jsonp',
     contentType: 'application/json; charset=utf-8',
     error: function (err) {
-        $('#mc-embedded-subscribe').text('Early Access');
+        $(buttonId).text(buttonText);
         if(err.status === 200) {
-          $('#message').empty();
-          $('#message').append('<div class="ta sr message-right">Way to huddle! 🚀</div>');
-          $('#mce-EMAIL').removeClass('z').addClass('tt');
-          $('#mce-EMAIL').val('')
+          $(messageContainerId).empty();
+          $(messageContainerId).append(`<div class="ta sr ${isRight ? 'message-right' : '' }">Way to huddle! 🚀</div>`);
+          $(inputId).removeClass('z').addClass('tt');
+          $(inputId).val('')
         } else {
-          $('#message').empty();
-          $('#message').append('<div class="ta st message-right">Something seems off. We\'re working on this.</div>');
-          $('#mce-EMAIL').removeClass('tt').addClass('z');
+          $(messageContainerId).empty();
+          $(messageContainerId).append(`<div class="ta st ${isRight ? 'message-right' : '' }">Something seems off. We\'re working on this.</div>`);
+          $(inputId).removeClass('tt').addClass('z');
         }
      },
     success: function (data) {
-        $('#mc-embedded-subscribe').text('Early Access');
+        $(buttonId).text(buttonText);
       if (data.result === 'success') {
-          $('#message').empty();
-          $('#message').append('<div class="ta sr message-right">Way to huddle! 🚀</div>');
-          $('#mce-EMAIL').removeClass('z').addClass('tt');
-          $('#mce-EMAIL').val('')
+          $(messageContainerId).empty();
+          $(messageContainerId).append(`<div class="ta sr ${isRight ? 'message-right' : '' }">Way to huddle! 🚀</div>`);
+          $(inputId).removeClass('z').addClass('tt');
+          $(inputId).val('')
       } else {
-      $('#message').empty();
-        $('#message').append('<div class="ta st message-right">Something seems off. We\'re working on this.</div>');
-        $('#mce-EMAIL').removeClass('tt').addClass('z');
+      $(messageContainerId).empty();
+        $(messageContainerId).append(`<div class="ta st ${isRight ? 'message-right' : '' }">Something seems off. We\'re working on this.</div>`);
+        $(inputId).removeClass('tt').addClass('z');
       }
     }
   })
