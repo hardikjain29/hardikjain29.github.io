9ea0aacbd31220910ab0297a8eb7e3ad414bcfce
Hardik Jain <29hdkjain@gmail.com>
2020-03-06 18:38:55 +0530

    Integrate mailchimp

--- mailchimp.js
+++ mailchimp.js
@@ -1,40 +1,61 @@
 $(document).ready(function () {
-    console.log('ex');
-    var $form = $('#mc-embedded-subscribe-form')
-    if ($form.length > 0) {
-      $('form button[type="submit"]').bind('click', function (event) {
-        if (event) event.preventDefault()
-        register($form)
-      })
-    }
-  })
-  
-  function register($form) {
-    $('#mc-embedded-subscribe').text('Listing you...');
-    $.ajax({
-      type: $form.attr('method'),
-      url: $form.attr('action'),
-      data: $form.serialize(),
-      cache: false,
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
-      error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
-      success: function (data) {
-        $('#mc-embedded-subscribe').text('Listed')
-        if (data.result === 'success') {
-          // Yeahhhh Success
-          console.log(data.msg)
-          $('#mce-EMAIL').css('borderColor', '#ffffff')
-          $('#subscribe-result').css('color', 'rgb(53, 114, 210)')
-          $('#subscribe-result').html('<p>Thank you for subscribing. We have sent you a confirmation email.</p>')
-          $('#mce-EMAIL').val('')
-        } else {
-          // Something went wrong, do something to notify the user.
-          console.log(data.msg)
-          $('#mce-EMAIL').css('borderColor', '#ff8282')
-          $('#subscribe-result').css('color', '#ff8282')
-          $('#subscribe-result').html('<p>' + data.msg.substring(4) + '</p>')
-        }
+  console.log('ex');
+  var $form = $('#mc-embedded-subscribe-form')
+  if ($form.length > 0) {
+    $('form button[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault();
+      if (validateEmail($('#mce-EMAIL').val())) {
+          register($form)
+      } else {
+          $('#message').empty();
+          $('#message').append('<div class="ta st message-right">Something seems off with the email.</div>');
+          $('#mce-EMAIL').removeClass('tt').addClass('z');
       }
     })
-  };
+  }
+})
+
+function validateEmail(email) 
+  {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+  }
+
+
+function register($form) {
+  $('#mc-embedded-subscribe').text('Listing you...');
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache: false,
+    dataType: 'jsonp',
+    contentType: 'application/json; charset=utf-8',
+    error: function (err) {
+        $('#mc-embedded-subscribe').text('Early Access');
+        if(err.status === 200) {
+          $('#message').empty();
+          $('#message').append('<div class="ta sr message-right">Way to huddle! 🚀</div>');
+          $('#mce-EMAIL').removeClass('z').addClass('tt');
+          $('#mce-EMAIL').val('')
+        } else {
+          $('#message').empty();
+          $('#message').append('<div class="ta st message-right">Something seems off. We\'re working on this.</div>');
+          $('#mce-EMAIL').removeClass('tt').addClass('z');
+        }
+     },
+    success: function (data) {
+        $('#mc-embedded-subscribe').text('Early Access');
+      if (data.result === 'success') {
+          $('#message').empty();
+          $('#message').append('<div class="ta sr message-right">Way to huddle! 🚀</div>');
+          $('#mce-EMAIL').removeClass('z').addClass('tt');
+          $('#mce-EMAIL').val('')
+      } else {
+      $('#message').empty();
+        $('#message').append('<div class="ta st message-right">Something seems off. We\'re working on this.</div>');
+        $('#mce-EMAIL').removeClass('tt').addClass('z');
+      }
+    }
+  })
+};
